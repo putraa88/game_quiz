85a58240a03beb2669deb7e8338c8aa6aeec01a4
putraa88 <mrdputra88@gmail.com>
2023-03-09 15:24:14 +0700

    quiz game completed

--- quiz.js
+++ quiz.js
@@ -1,2 +1,65 @@
-// intial commit
-const changeVariable = 'development';
+const prompt = require('prompt-sync')();
+
+// prompt user to input name for both players
+const playerOneName = prompt('Enter player one name: ');
+const playerTwoName = prompt('Enter player two name: ');
+
+console.log(`Welcome ${playerOneName} and ${playerTwoName}`);
+
+// varibale for quiz
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let round = 1;
+
+// loop for the game (max of 5 rounds)
+while(round <= 5) {
+    console.log(`START ROUND: ${round}`);
+
+    // generate correct answer
+    const correctAnswer = Math.ceil(Math.random() * 3);
+
+    const playerOneAnswer = prompt(`${playerOneName} please enter the number between 1 - 3: `);
+    // validate player 1 answer
+    if (playerOneAnswer < 1 || playerOneAnswer > 3) {
+        console.log('Incorrect number!')
+        continue;
+    }
+    const playerTwoAnswer = prompt(`${playerTwoName} please enter the number between 1 - 3: `);
+    // valudate player 2 answer
+    if (playerTwoAnswer < 1 || playerTwoAnswer > 3) {
+        console.log('Incorrect number!')
+        continue;
+    }
+
+    // validate both answer cannot be the same
+    if (playerOneAnswer === playerTwoAnswer) {
+        console.log('Both players cannot have the same answer');
+        continue;
+    }
+
+    // determine winner
+    if (playerOneAnswer == correctAnswer) {
+        console.log(`${playerOneName} wins round ${round}`);
+        playerOneScore ++;
+    } else if (playerTwoAnswer == correctAnswer) {
+        console.log(`${playerOneName} wins round ${round}`);
+        playerTwoScore ++;
+    } else {
+        console.log(`It's a DRAW!`);
+    }
+
+    console.log(`ROUND ${round} results`);
+    console.log(`${playerOneName} - ${playerOneScore}`);
+    console.log(`${playerTwoName} - ${playerTwoScore}`);
+    
+    if (round < 5) {
+        const nextGame = prompt('Continue? (Y/N): ');
+        if (nextGame.toUpperCase() == 'Y') {
+            round ++;
+        } else {
+            break;
+        }
+    } else {
+        break;
+    }
+}
